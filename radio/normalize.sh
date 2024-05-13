bc290ff9ca9c3f0c1cdfc7cd992e19b8699b43f3
WEBERJR7494 <weberjr7494@cn02.head.bose.hpc.cluster>
2024-05-13 17:34:51 -0500

    updated results

--- radio/normalize.sh
+++ radio/normalize.sh
@@ -10,13 +10,13 @@ num_runs=50
 for ((i=1; i<=$num_runs; i++)); do
     echo "Running NS2 simulation #$i"
     which ns
-#    ns ~/my_cs462/ns-project/radio/shadowing_indoor_opaque_rand.tcl       
+    ns ~/my_cs462/ns-project/radio/shadowing_indoor_opaque_rand.tcl       
     ns ~/my_cs462/ns-project/radio/shadowing_indoor_transparent_rand.tcl       
-#    ns ~/my_cs462/ns-project/radio/shadowing_outdoor_rand.tcl       
+    ns ~/my_cs462/ns-project/radio/shadowing_outdoor_rand.tcl       
 
     echo "NS2 simulation #$i completed"
-#    awk -f awk-scripts/throughput2.awk ~/my_cs462/ns-project/radio/tr-files/shadowing_outdoor_rand.tr >> ~/my_cs462/ns-project/radio/norm-data/outdoor.data
-#    awk -f awk-scripts/throughput2.awk ~/my_cs462/ns-project/radio/tr-files/shadowing_indoor_opaque_rand.tr >> ~/my_cs462/ns-project/radio/norm-data/indoor_o.data
+    awk -f awk-scripts/throughput2.awk ~/my_cs462/ns-project/radio/tr-files/shadowing_outdoor_rand.tr >> ~/my_cs462/ns-project/radio/norm-data/outdoor.data
+    awk -f awk-scripts/throughput2.awk ~/my_cs462/ns-project/radio/tr-files/shadowing_indoor_opaque_rand.tr >> ~/my_cs462/ns-project/radio/norm-data/indoor_o.data
     awk -f awk-scripts/throughput2.awk ~/my_cs462/ns-project/radio/tr-files/shadowing_indoor_transparent_rand.tr >> ~/my_cs462/ns-project/radio/norm-data/indoor_t.data
 done
 
